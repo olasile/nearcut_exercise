b2cf974db413ed4477fba08e8e0a090d3fda9117
Shile Fasugba <shilefasugba@Shiles-MacBook-Pro.local>
2024-06-07 14:45:36 -0400

    Added User model specs

---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { 'Test User' }
+    password { 'Aqpfk1swods' }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { build(:user)}
+
+  context 'valid attributes' do
+    it 'is valid with valid attributes' do
+      user = User.new(name: 'Test User', password: 'Aqpfk1swods')
+      expect(user).to be_valid
+    end
+  end
+
+  context 'invalid attributes' do
+    it 'is invalid without a name' do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid without a password' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid with a short password' do
+      user.password = 'Abc123'
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid without an uppercase character or digit' do
+      user.password = 'abcdefghijklmnop'
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid with a password containing three repeating characters' do
+      user.password = 'AAAfk1swods'
+      expect(user).not_to be_valid
+    end
+  end
+end
